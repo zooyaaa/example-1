ca00bb4b911e4a8d9d3115dcf30b4e64aaa91c23
zooyaaa <88900900+zooyaaa@users.noreply.github.com>
2021-08-13 13:04:23 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var rect;
+rect = createsprite(1,2,30,10);
+rect.shapeColor = "red";
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw()
+{
+  background(30);
+  drawSprites();
+}
+
+
+
+
